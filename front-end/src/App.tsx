d6e986b348a3cbb57651125c5c7102289d369132
Gentian Sadiku <sadikugentian@gmail.com>
2022-07-17 18:03:24 +0200

    Implement routes

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Home from "./Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>NFT Minting App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
